989b8225005a86c4999b9b2e03444be856fb89b6
Kevin Ndaya <ndayakevin2007@gmail.com>
2026-08-09 23:42:40 +0100

    Detection des valeurs à remplacer

---
+++ pipeline.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("data/global_freelancers_raw.csv")
+
+# Valeurs à remplacer dans les colonnes du dataframe
+mapping_gender = {
+    'F' : 'F',
+    'FEMALE' : 'F',
+    'M' : 'M',
+    'MALE' : 'M',
+}
+
+mapping_is_active = {
+    '0': False,
+    '1' : True,
+    'N' : False, 
+    'False' : False,
+    'True' : True,
+    'yes' : True,
+    'Y' : True,
+    'nan' : False,
+    'no' : False
+}
+
